e8768c00477d7b5e508819d1c172ff554d1b7933
Fanta <a01638657@tec.mx>
2025-03-12 18:09:10 -0600

    next

--- app.java
+++ app.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 public class app {
     public static void main(String[] args) {
         port(8080);  // Start server on port 8080
-        get("/", (req, res) -> "Hello World!");  // Return "Hello World!"
+        get("/", (req, res) -> "Hola");  // Return "Hello World!"
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Shutting down Spark server...");
