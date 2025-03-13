531a4e473a3882fb9a460476551c55e80b46000f
Fanta <a01638657@tec.mx>
2025-03-12 19:49:04 -0600

    it didn't show the website

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import static spark.Spark.port;
+import static spark.Spark.get;
+import static spark.Spark.stop;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from World!");
+        port(8080);
+        get("/", (req, res) -> "Hello from World!");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down Spark server...");
+            stop();
+        }));
+
     }
 
     // Simple method to test
     public static String getGreeting() {
         return "Hello from World!";
     }
+
+    
 }
